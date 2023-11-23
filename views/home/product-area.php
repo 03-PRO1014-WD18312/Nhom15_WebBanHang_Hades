bf803e4686843efe1bd2795c94348284f9b22cce
haihm01 <haihmph42719@fpt.edu.vn>
2023-11-23 14:13:27 +0700

    push_hai_admin

--- views/home/product-area.php
+++ views/home/product-area.php
@@ -1,83 +1,99 @@
 <!-- Start Product Area -->
 <div class="product-area section">
-            <div class="container">
-				<div class="row">
-					<div class="col-12">
-						<div class="section-title">
-							<h2>Trending Item</h2>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-12">
-						<div class="product-info">
-							<div class="nav-main">
-								<!-- Tab Nav -->
-								<ul class="nav nav-tabs" id="myTab" role="tablist">
-									<?php foreach ($listCate as $list):?>
-										<?php if($list['category_name'] == "Tops"):?>
-											<li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#<?=$list['category_name']?>" data-cateid="<?=$list['id']?>" role="tab"><?=$list['category_name']?></a></li>
-											<?php else :?>
-											<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#<?=$list['category_name']?>" data-cateid="<?=$list['id']?>" role="tab"><?=$list['category_name']?></a></li>
-											<?php endif;?>
-									<?php endforeach;?>
-									<!-- <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#women" role="tab">Woman</a></li>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h2>Trending Item</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="product-info">
+                    <div class="nav-main">
+                        <!-- Tab Nav -->
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <?php foreach ($listCate as $list):?>
+                            <?php if($list['category_name'] == "Tops"):?>
+                            <li class="nav-item"><a class="nav-link active" data-toggle="tab"
+                                    href="#<?=$list['category_name']?>" data-cateid="<?=$list['id']?>"
+                                    role="tab"><?=$list['category_name']?></a></li>
+                            <?php else :?>
+                            <li class="nav-item"><a class="nav-link" data-toggle="tab"
+                                    href="#<?=$list['category_name']?>" data-cateid="<?=$list['id']?>"
+                                    role="tab"><?=$list['category_name']?></a></li>
+                            <?php endif;?>
+                            <?php endforeach;?>
+                            <!-- <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#women" role="tab">Woman</a></li>
 									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#kids" role="tab">Kids</a></li>
 									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#accessories" role="tab">Accessories</a></li>
 									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#essential" role="tab">Essential</a></li>
 									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#prices" role="tab">Prices</a></li> -->
-								</ul>
-								<!--/ End Tab Nav -->
-							</div>
-							<div class="tab-content" id="myTabContent">
-								<!-- Start Single Tab -->
-						<?php for ($i = 0; $i < count($listCate); $i++): ?>
-						<div class="tab-pane fade <?=($i === 0) ? 'show active' : ''?>" id="<?=$listCate[$i]['category_name']?>" role="tabpanel">
-							<div class="tab-single">
-								<div class="row">
-									<?php foreach ($listPro as $pro): ?>
-										<?php if ($pro['category'] == $listCate[$i]['id']): ?>
-											<div class="col-xl-3 col-lg-4 col-md-4 col-12">
-												<div class="single-product">
-													<div class="product-img">
-															<a href="../views/indexProdetail.php?id=<?=$pro['product_id']?>&act=loadOne&name=<?=$listCate[$i]['category_name']?>">
-															<img class="default-img" src="../images/<?=$listCate[$i]['category_name']?>/<?=$pro['product_image']?>" alt="#">
-															<img class="hover-img" src="../images/<?=$listCate[$i]['category_name']?>/<?=$pro['image_dt1']?>" alt="#">
-														</a>
-														<div class="button-head">
-															<div class="product-action">
-																<a class="view-detail" data-id="<?php echo $pro['product_id'] ?>" title="Quick View"><i class=" ti-eye"></i><span>Quick Shop</span></a>
-																<a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add to Wishlist</span></a>
-																<a title="Compare" href="#"><i class="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
-															</div>
-															<div class="product-action-2">
-															<?php if($pro['product_qty'] == 0): ?>
-															<del>Add to cart</del>
-															<?php else:?>
-																<a title="Add to cart" href="#">Add to cart</a>
-															<?php endif;?>
-																
-															</div>
-														</div>
-													</div>
-													<div class="product-content">
-														<h3><a href="../views/indexProdetail.php?id=<?=$pro['product_id']?>&act=loadOne&name=<?=$listCate[$i]['category_name']?>"><?=$pro['product_name']?></a></h3>
-														<div class="product-price">
-															<span><?=number_format($pro['product_price'])?>VNĐ</span>
-														</div>
-													</div>
-												</div>
-											</div>
-										<?php endif;?>
-									<?php endforeach;?>
-								</div>
-							</div>
-						</div>
-					<?php endfor;?>
+                        </ul>
+                        <!--/ End Tab Nav -->
+                    </div>
+                    <div class="tab-content" id="myTabContent">
+                        <!-- Start Single Tab -->
+                        <?php for ($i = 0; $i < count($listCate); $i++): ?>
+                        <div class="tab-pane fade <?=($i === 0) ? 'show active' : ''?>"
+                            id="<?=$listCate[$i]['category_name']?>" role="tabpanel">
+                            <div class="tab-single">
+                                <div class="row">
+                                    <?php foreach ($listPro as $pro): ?>
+                                    <?php if ($pro['category'] == $listCate[$i]['id']): ?>
+                                    <div class="col-xl-3 col-lg-4 col-md-4 col-12">
+                                        <div class="single-product">
+                                            <div class="product-img">
+                                                <a
+                                                    href="../views/indexProdetail.php?id=<?=$pro['product_id']?>&act=loadOne&name=<?=$listCate[$i]['category_name']?>">
+                                                    <img class="default-img" src="../images/<?=$pro['product_image']?>"
+                                                        alt="#">
+                                                    <img class="hover-img" src="../images/<?=$pro['image_dt1']?>"
+                                                        alt="#">
+                                                </a>
+                                                <div class="button-head">
+                                                    <div class="product-action">
+                                                        <a class="view-detail"
+                                                            data-id="<?php echo $pro['product_id'] ?>"
+                                                            title="Quick View"><i class=" ti-eye"></i><span>Quick
+                                                                Shop</span></a>
+                                                        <a title="Wishlist" href="#"><i class=" ti-heart "></i><span>Add
+                                                                to Wishlist</span></a>
+                                                        <a title="Compare" href="#"><i
+                                                                class="ti-bar-chart-alt"></i><span>Add to
+                                                                Compare</span></a>
+                                                    </div>
+                                                    <div class="product-action-2">
+                                                        <?php if($pro['product_qty'] == 0): ?>
+                                                        <del>Add to cart</del>
+                                                        <?php else:?>
+                                                        <a title="Add to cart" href="#">Add to cart</a>
+                                                        <?php endif;?>
 
-								<!--/ End Single Tab -->
-								<!-- Start Single Tab -->
-								<!-- <div class="tab-pane fade" id="Bottoms" role="tabpanel">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="product-content">
+                                                <h3><a
+                                                        href="../views/indexProdetail.php?id=<?=$pro['product_id']?>&act=loadOne&name=<?=$listCate[$i]['category_name']?>"><?=$pro['product_name']?></a>
+                                                </h3>
+                                                <div class="product-price">
+                                                    <span><?=number_format($pro['product_price'])?>VNĐ</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php endif;?>
+                                    <?php endforeach;?>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endfor;?>
+
+                        <!--/ End Single Tab -->
+                        <!-- Start Single Tab -->
+                        <!-- <div class="tab-pane fade" id="Bottoms" role="tabpanel">
 									<div class="tab-single">
 										<div class="row">
 											<div class="col-xl-3 col-lg-4 col-md-4 col-12">
@@ -295,9 +311,9 @@
 										</div>
 									</div>
 								</div> -->
-								<!--/ End Single Tab -->
-								<!-- Start Single Tab -->
-								<!-- <div class="tab-pane fade" id="Outwears" role="tabpanel">
+                        <!--/ End Single Tab -->
+                        <!-- Start Single Tab -->
+                        <!-- <div class="tab-pane fade" id="Outwears" role="tabpanel">
 									<div class="tab-single">
 										<div class="row">
 											<div class="col-xl-3 col-lg-4 col-md-4 col-12">
@@ -515,9 +531,9 @@
 										</div>
 									</div>
 								</div> -->
-								<!--/ End Single Tab -->
-								<!-- Start Single Tab -->
-								<!-- <div class="tab-pane fade" id="Footewears" role="tabpanel">
+                        <!--/ End Single Tab -->
+                        <!-- Start Single Tab -->
+                        <!-- <div class="tab-pane fade" id="Footewears" role="tabpanel">
 									<div class="tab-single">
 										<div class="row">
 											<div class="col-xl-3 col-lg-4 col-md-4 col-12">
@@ -735,9 +751,9 @@
 										</div>
 									</div>
 								</div> -->
-								<!--/ End Single Tab -->
-								<!-- Start Single Tab -->
-								<!-- <div class="tab-pane fade" id="Hats" role="tabpanel">
+                        <!--/ End Single Tab -->
+                        <!-- Start Single Tab -->
+                        <!-- <div class="tab-pane fade" id="Hats" role="tabpanel">
 									<div class="tab-single">
 										<div class="row">
 											<div class="col-xl-3 col-lg-4 col-md-4 col-12">
@@ -955,9 +971,9 @@
 										</div>
 									</div>
 								</div> -->
-								<!--/ End Single Tab -->
-								<!-- Start Single Tab -->
-								<!-- <div class="tab-pane fade" id="Bags" role="tabpanel">
+                        <!--/ End Single Tab -->
+                        <!-- Start Single Tab -->
+                        <!-- <div class="tab-pane fade" id="Bags" role="tabpanel">
 									<div class="tab-single">
 										<div class="row">
 											<div class="col-xl-3 col-lg-4 col-md-4 col-12">
@@ -1175,19 +1191,19 @@
 										</div>
 									</div>
 								</div> -->
-								<!--/ End Single Tab -->
-								<div class="pagination">
-												<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>
-												<a href="#" class="active">1</a>
-												<a href="#">2</a>
-												<a href="#">3</a>
-												<a href="#" class="dot-dot"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
-												<a href="#">6</a>
-												<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+                        <!--/ End Single Tab -->
+                        <div class="pagination">
+                            <a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>
+                            <a href="#" class="active">1</a>
+                            <a href="#">2</a>
+                            <a href="#">3</a>
+                            <a href="#" class="dot-dot"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+                            <a href="#">6</a>
+                            <a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
     </div>
+</div>
